6c44b70f3459f2fbc6b47b8647cd592e2395bc16
Alghifari Maula Abi <alghifarimaulaabi@gmail.com>
2026-06-21 17:29:07 +0700

    component navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <>
-    <h1 className="text-red-700 text-4xl font-bold">test commit</h1>
+    <h1 className="text-3xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt doloribus, voluptate neque exercitationem quasi dolorem consectetur magnam ullam eaque, repellendus itaque soluta ad vero officia amet cum culpa asperiores tempora reiciendis vitae harum? Repellat vitae aliquid ipsam suscipit officiis quidem exercitationem earum soluta quod ipsum vel, consequuntur pariatur enim labore porro sed totam quo, natus iusto ab cumque animi beatae. Animi eveniet excepturi assumenda unde corporis provident, ad reiciendis, autem voluptatum vero suscipit ab at tempore, deleniti placeat fuga possimus natus iusto ut rerum ratione non? Nemo, error voluptas excepturi repellat impedit repudiandae, quaerat atque blanditiis unde quos expedita repellendus id enim et animi obcaecati minima dolor qui. Distinctio commodi eum inventore quod perferendis molestias aperiam voluptatum est sint vel iste, soluta quidem enim explicabo iure animi dolores nulla doloremque tempore? Atque excepturi aut voluptatibus quibusdam modi tempore dolorum laboriosam iste illo est nihil voluptatem libero unde illum, earum natus neque nulla optio! Voluptatem vero similique distinctio a accusantium mollitia dignissimos? Pariatur accusantium quia magni dolores deserunt illo et eveniet aliquid, temporibus aut vel sit eos fugiat sunt quas tempore, illum velit blanditiis nemo saepe quae. Tenetur, ratione! Suscipit aut, deleniti nisi consectetur quidem enim nihil veritatis consequuntur mollitia facilis!</h1>
     </>
   )
 }
